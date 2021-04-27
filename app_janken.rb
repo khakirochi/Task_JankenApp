76b050b03f4c3cda7ee2069714ee9bbc5db31024
khakirochi <khakirochi@users.noreply.github.com>
2021-04-28 06:51:27 +0900

    ジャンケンの勝者判定じにcase-when-endでSetの値で分岐させていたが Rubyのバージョンによって===の挙動が異なり，正常に動作しなかったため Setの代わりに配列をベースに判定するようにロジックを変更：
    ```
    Set[0,1] === Set[1,0]  # true(v2.3), false(v2.6)
    ```
    
    合わせて全体の設計を以下のように変更：
    ・ゲームのプレイヤーのクラスを定義し，"あなた"と”相手”に対応するインスタンスを生成
    ・各ゲーム(ジャンケン，あっち向いてホイ)に対応するクラスを定義し，
    　それぞれのインスタンスを生成して使用

--- app_janken.rb
+++ app_janken.rb
@@ -1,43 +1,57 @@
-require 'set'
 
 module HandType
-  ROCK     = 0
-  PAPER    = 1
-  SCISSORS = 2
+  ROCK     = 0  # グー
+  SCISSORS = 1  # チョキ
+  PAPER    = 2  # パー
 end
 
 module Direction
-  UP    = 0
-  DOWN  = 1
-  LEFT  = 2 
-  RIGHT = 3
+  UP    = 0  # 上
+  DOWN  = 1  # 下
+  LEFT  = 2  # 左
+  RIGHT = 3  # 右
 end
 
+HANDTYPE_NAMES  = {ROCK:"グー", SCISSORS:"チョキ", PAPER:"パー"}
+DIRECTION_NAMES = {UP:"上", DOWN:"下", LEFT:"左", RIGHT:"右"}
+
+# ジャンケンのプレイヤーを現すクラス．
 class JankenGamePlayer 
   include Comparable
-  include HandType
 
+  # ジャンケンのプレイヤー名のシンボル
+  attr_reader   :name
+  # 勝者フラグ (true:勝，false:負)
   attr_accessor :is_winner
+  # プレイヤーの出し手を表す正の整数
   attr_accessor :input
 
-  def initialize
+  def initialize(name)
+    @name = name
     @is_winner = false
     @input = nil
   end
 
+  # inputを初期化(nil)するメソッド
+  def reset_input
+    @input = nil
+  end
+
+  include HandType
+  # ジャンケンの勝負けに対応して比較(勝>負)可能とするための演算子定義
   def <=>(other)
     players = [self, other]
     inputs = players.map(&:input)
-
     if inputs.any?(&:nil?)
       nil
     else
       if inputs.uniq.length==2
-        case inputs.to_set
-        when Set[HandType::ROCK,     HandType::SCISSORS] then self.input  - other.input  #0:グー,    1:チョキ
-        when Set[HandType::SCISSORS, HandType::PAPER]    then other.input - self.input   #1:チョキ,  2:パー
-        when Set[HandType::PAPER,    HandType::ROCK]     then other.input - self.input   #2:パー,    0:グー
-        else nil
+        case inputs.sort
+        when [HandType::ROCK,     HandType::SCISSORS].sort then other.input - self.input   #0:グー   <=>  1:チョキ -> 1以上の整数を返す.
+        when [HandType::SCISSORS, HandType::PAPER].sort    then other.input - self.input   #1:チョキ <=>  2:パー   -> 1以上の整数を返す.
+        when [HandType::PAPER,    HandType::ROCK].sort     then self.input - other.input   #2:パー   <=>  0:グー   -> 1以上の整数を返す.
+        else
+          nil
         end
       else
         0 #あいこ
@@ -46,19 +60,34 @@ class JankenGamePlayer
   end
 end
 
+# 各ゲームのスーパークラス
 class Game
+  # ゲームのプレイヤー(JankenGamePlayerのインスタンス)の配列
   attr_accessor  :players
 
+  def initialize(players, choices)
+    @players = players
+    @choices = choices  # ["グー", "チョキ", "パー", "戦わない"], ["上","下","左","右"]
+  end
+
+  # ゲームを実行するメソッド
   def play
+    # 実行前の処理
     preprocess
+    # 掛け声
     shout
+    # 結果処理
     postprocess
   end
 
-  def reset_inputs
-    @players.map{|p|p.input = nil}
+  # 各プレイヤーが洗濯した出し手を出力するメソッド
+  def print_players_choices
+    puts "-----"*10
+    puts @players.inject(""){|r, p| r + "#{p.name}\t : #{@choices[p.input]}\n"}
+    puts "-----"*10
   end
-  
+
+  # 0-3の入力を要求し，入力結果の整数を返すメソッド
   def ask_input_0_3
     input_raw = gets.chomp
     until input_raw.match(/^[0-3]$/)
@@ -68,191 +97,194 @@ class Game
     input_raw.to_i
   end
 
-  def generate_input(auto=false)
+  # プレイヤーの入力を整数形式で生成するメソッド
+  def generate_input(random=false)
     raise NotImplementedError
   end
 
+  # ゲーム開始時のメッセージを出力するメソッド
   def print_start_message
     raise NotImplementedError
   end
 
+  # ゲーム実行前の処理を実行するメソッド
   def preprocess
     raise NotImplementedError
   end
 
-  def print_shout
+  # ゲーム実行時の掛け声を表示するメソッド
+  def shout
     raise NotImplementedError
   end
 
+  # ゲームの結果処理を行うメソッド，ゲームのプレイヤーの勝者フラグの更新を行う
   def postprocess
-    raise NotImplementedError
-  end
-
-  def print_result
     raise NotImplementedError
   end
 
 end
 
+# ジャンケンゲームのクラス
 class JankenGame < Game
-  include HandType
-  HANDTYPE_NAMES = {ROCK:"グー", PAPER:"チョキ", SCISSORS:"パー"}
-
-  attr_reader :SHOUT_MESSAGE
-
-  def generate_input(auto=false)
-    if auto
-      rand(0..2)
+  # ジャンケンゲームの入力を生成するメソッド
+  def generate_input(random=false)
+    if random
+      rand(0..2) # ランダムな整数入力は0-2の範囲で生成
     else
-      ask_input_0_3
+      ask_input_0_3 # それ以外の場合には0-3の範囲の入力を受け取る
     end
   end
 
+  # ジャンケンゲーム開始時のメッセージを出力するメソッド．合わせて対応する掛け声も指定．
   def print_start_message
     if @players.map(&:input).any?(&:nil?)
-      puts "じゃんけん..."
+      puts "じゃんけん..."      # ゲームのプレイヤーの入力が未入力(nil)の時は，ジャンケン開始時と判断
       @SHOUT_MESSAGE = "ホイ"
     else
-      puts "あいこで..."
+      puts "あいこで..."    #　それ以外はあいこの状態を判断 
       @SHOUT_MESSAGE = "ショ"
     end
 
-    choises = HandType.constants.map{|key|HANDTYPE_NAMES[key]} << "戦わない"
-    puts choises.each.with_index.inject(""){|res, (val, idx)| res + "#{idx}(#{val}) "}
+    # プレイヤーが指定できる出し手の選択肢を表示
+    puts (@choices).each.with_index.inject(""){|r, (val, idx)| r + "#{idx}(#{val}) "}
   end
 
+  # ジャンケンゲーム実行前の処理．
   def preprocess
-    if players[0].input == 3 then exit end
+    # プレイヤーから"戦わない(3)"が指定されたら終了する
+    if players.map(&:input).include?(3) then exit end
   end
 
+  # ジャンケンゲームの掛け声
   def shout
     puts @SHOUT_MESSAGE
   end
 
+  # ジャンケンゲームの勝者フラグを更新するメソッド
   def postprocess
+    # 勝者が存在する場合，どちらか一方のみの勝者フラグ(is_winner)がtrueになる
     @players[0].is_winner = (@players[0] > @players[1])
     @players[1].is_winner = (@players[0] < @players[1])
   end
 
-  def print_result
-    puts "--------------------------------------"
-    puts "あなた  : #{HANDTYPE_NAMES[HandType.constants[players[0].input]]}を出しました"
-    puts "相手　  : #{HANDTYPE_NAMES[HandType.constants[players[1].input]]}を出しました"
-  puts "--------------------------------------"
-  end
 end
 
+# あっち向いてホイのクラス
 class AhoiGame < Game
-    include Direction
-    
-    attr_reader :SHOUT_MESSAGE
-    DIRECTION_NAMES = {UP:"上", DOWN:"下", LEFT:"左", RIGHT:"右"}
-    
-    def generate_input(auto=false)
-      if auto
-        rand(0..3)
+  # あっち向いてホイの入力を生成するメソッド
+  def generate_input(random=false)
+      if random
+        rand(0..3)  # ランダムな整数入力は0-3の範囲で生成
       else
-        ask_input_0_3
+        ask_input_0_3 # それ以外の場合には0-3の範囲の入力を受け取る
       end
     end
   
-    def print_start_message
-      puts "あっち向いて〜"
+  # あっち向いてホイ開始時のメッセージを出力するメソッド．合わせて対応する掛け声も指定．
+  def print_start_message
+      if @players.find{|p|p.is_winner}.name == :あなた
+        your_mode = "指の向きを指定してください"
+      else
+        your_mode = "顔の向きを指定してください"
+      end
+      puts "あっち向いて〜\t(#{your_mode})"
       @SHOUT_MESSAGE = "ホイ"
 
-      choises = Direction.constants.map{|key|DIRECTION_NAMES[key]}
-      puts choises.each.with_index.inject(""){|res, (val, idx)| res + "#{idx}(#{val}) "}
+    # プレイヤーが指定できる出し手の選択肢を表示
+    puts @choices.each.with_index.inject(""){|res, (val, idx)| res + "#{idx}(#{val}) "}
     end
   
-    def preprocess
+   # あっち向いてホイ実行前の処理．あっち向いてホイの場合には特に何も行わない
+   def preprocess
       #nop
     end
   
+    # あっち向いてホイの掛け声
     def shout
       puts @SHOUT_MESSAGE
     end
-  
+
+    # あっち向いてホイの勝者フラグを更新するメソッド 
     def postprocess
+      # プレイヤーの入力が異なる場合，全プレイヤーの勝者フラグをfalseに更新する.
       inputs = @players.map(&:input)
       if inputs.uniq.length != 1
         @players.map{|p|p.is_winner = false}
       end
     end
-  
-    def print_result
-      puts "--------------------------------------"
-      puts "あなた  : #{DIRECTION_NAMES[Direction.constants[players[0].input]]}"
-      puts "相手　  : #{DIRECTION_NAMES[Direction.constants[players[1].input]]}"
-    puts "--------------------------------------"
-    end
+
 end
 
 
 
-#main
+# main
 
-#games
-jg = JankenGame.new
-ag = AhoiGame.new
+# ゲームのプレイヤーを生成
+players = [:あなた, :相手].map{|name|JankenGamePlayer.new(name)}
 
-#players
-p0 = JankenGamePlayer.new  # your player
-p1 = JankenGamePlayer.new  # opponent player
+# 各ゲームの出し手の選択肢
+jg_choices = HandType.constants.map{|ky| HANDTYPE_NAMES[ky]} << "戦わない"  # ["グー","チョキ","パー","戦わない"]
+ah_choices = Direction.constants.map{|ky| DIRECTION_NAMES[ky]}            # ["上","下","左","右"]
 
-#set game players
-players = [p0, p1]
-jg.players = ag.players = players
+# 各ゲームのインスタンスを生成
+jg = JankenGame.new(players, jg_choices)
+ag = AhoiGame.new(players, ah_choices)
 
-#do 
+
+# ゲームの実行を開始.勝者となるプレイヤーが発生するまで以下のループを実行する
 until players.map(&:is_winner).any?
-  #reset players' inputs
-  p0.input, p1.input = nil, nil
+  # 1. ジャンケン
+  # 各プレイヤーが保持している入力情報を初期化
+  players.map(&:reset_input)  
 
-  until players.map(&:is_winner).any?
-    jg.print_start_message
+  # 勝者が確定するまでジャンケンを繰り返す
+  until players.map(&:is_winner).any?  
+    jg.print_start_message # 開始時メッセージを表示
 
-    p0.input = jg.generate_input(auto=false)
-    p1.input = jg.generate_input(auto=true)
+    players.each do |p| # 各プレイヤーの入力を生成
+      if p.name == :あなた
+        p.input = jg.generate_input(random=false)
+      else
+        p.input = jg.generate_input(random=true)   # あなた以外の入力はランダムに生成 
+      end
+    end
 
-    puts "p0.input = #{p0.input}"
-    puts "p1.input = #{p1.input}"
-    puts "p0<=>p1 = #{p0<=>p1}"
-    
+    # ジャンケン実行
     jg.play
     
-    jg.print_result
+    # ジャンケン実行時の各プレイヤーの出力を表示
+    jg.print_players_choices
   end
 
-  puts "p0.is_winner = #{p0.is_winner}"
-  puts "p1.is_winner = #{p1.is_winner}"
+  # 2. あっち向いてホイ
+  # 各プレイヤーが保持している入力情報を初期化
+  players.map(&:reset_input)  
 
-  ag.print_start_message
+  # ジャンケンの勝者と敗者を取得
+  jk_winner, jk_loser = players.find{|p|p.is_winner}, players.find{|p|!p.is_winner} 
+  
+  # 開始時メッセージを表示
+  ag.print_start_message 
 
-  finger_direction, face_direction = nil, nil
-  if p0 > p1
-    finger_direction = ag.generate_input(auto=false)
-    face_direction   = ag.generate_input(auto=true)
-
-    p0.input = finger_direction
-    p1.input = face_direction
-  elsif p0 < p1
-    finger_direction = ag.generate_input(auto=true)
-    face_direction   = ag.generate_input(auto=false)
-
-    p0.input = face_direction
-    p1.input = finger_direction
+  # 勝者は指の向き，敗者は顔の向きを指定する
+  if jk_winner.name == :あなた
+    finger_direction = ag.generate_input(random=false)
+    face_direction   = ag.generate_input(random=true)
+  else
+    finger_direction = ag.generate_input(random=true)
+    face_direction   = ag.generate_input(random=false)
   end
 
+  jk_winner.input = finger_direction
+  jk_loser.input  = face_direction
+
+  # あっち向いてホイ実行
   ag.play
-  ag.print_result
+
+  # ジャンケン実行時の各プレイヤーの出力を表示
+  ag.print_players_choices
 
 end
 
-if p0.is_winner && !p1.is_winner
-  puts "あなたの勝ちです"
-elsif !p0.is_winner && p1.is_winner
-  puts "相手の勝ちです"
-else
-  puts "(勝者不明です)"
-end
+puts players.select{|p|p.is_winner}.inject("勝者: "){|r,p|r + p.name.to_s + ","}.chomp(",")
 
